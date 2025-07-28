c16d3256e66df4193e290f5c0ad2a29dfcc9847d
prasadgd2004 <prasadgd04@gmail.com>
2025-07-28 11:49:16 +0530

    dp

---
+++ c_practice/gd1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int dp;
+	printf("enter the weight:");
+	scanf("%d",&dp);
+
+	(dp<=60)? printf("playing kabbadi"):printf("not playing kabbadi");
+	return 0;
+}
