33695b5b3cc0fc262f74dab51fa9ff9a5dc2689e
prasadgd2004 <prasadgd04@gmail.com>
2025-07-29 11:21:28 +0530

    dp1

---
+++ c_practice/data/d2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int i=10;
+	char a;
+	float x=2.5;
+	double y=4.55;
+	printf("%d\n %c\n %f\n %lf\n", i, a,x,y);
+	return 0;
+}
---
+++ c_practice/data/oper.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int a=1;
+	char dp='B';
+	float x=5.4;
+	double y=6.43;
+	printf("%d\n %c\n %f\n %lf\n",a,dp,x,y);
+       	return 0;
+}
