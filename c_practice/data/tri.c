ef834dc6674f879c0ff4fd79bb9f14e30883c948
prasadgd2004 <prasadgd04@gmail.com>
2025-08-01 17:35:47 +0530

    dp14

---
+++ c_practice/data/tri.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+        double a,b;
+	printf("enter the number:");
+       	scanf("%lf%lf",&a,&b);
+	double c=180-(a+b);
+	printf("third angle:%lf\n",c);
+	return 0;
+}
+
