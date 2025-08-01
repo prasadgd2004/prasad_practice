9365efaa33105cc2b12bfcc1b21b15d99072887f
prasadgd2004 <prasadgd04@gmail.com>
2025-08-01 17:07:32 +0530

    dp12

---
+++ c_practice/data/sq.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a,b=0.5, result;
+	printf("enter the number:");
+	scanf("%f",&a);
+	result = pow(a,b);
+	printf("squareroot of number:%f\n",result);
+	return 0;
+}
+
